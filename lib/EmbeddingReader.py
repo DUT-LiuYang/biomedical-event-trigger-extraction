1d9c560167df271a4b6d8b124ce7f4eb46345976
DUT-LiuYang <1396924332@qq.com>
2018-10-17 20:51:35 +0800

    增加构建词向量矩阵的代码

---
+++ lib/EmbeddingReader.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pickle
+
+
+class EmbeddingReader(object):
+
+    def __init__(self):
+        self.dir = "../resource/"
+        self.WORD_EMBEDDING_DIM = 200
+        self.ENTITY_EMBEDDING_DIM = 50
+
+        self.word_embedding_file = self.dir + "dim200vecs"
+
+    def trim_word_embedding(self, word_num=20000, word_index={}):
+        rf = open(self.word_embedding_file, 'r', encoding='utf-8')
+
+        embeddings_index = {}
+
+        for line in rf:
+            values = line.split()
+            index = len(values) - self.WORD_EMBEDDING_DIM
+            if len(values) > (self.WORD_EMBEDDING_DIM + 1):
+                word = ""
+                for i in range(len(values) - self.WORD_EMBEDDING_DIM):
+                    word += values[i] + " "
+                word = word.strip()
+            else:
+                word = values[0]
+            # print(line)
+            coefs = np.asarray(values[index:], dtype='float32')
+            embeddings_index[word] = coefs
+
+        rf.close()
+
+        num_words = min(word_num, len(word_index))
+        print("word num: " + str(num_words))
+        embedding_matrix = np.zeros((num_words + 2, self.WORD_EMBEDDING_DIM))
+        for word, i in word_index.items():
+            if i >= word_num:
+                continue
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                # words not found in embedding index will be all-zeros.
+                embedding_matrix[i] = embedding_vector
+
+        wf = open(self.dir + "embedding_matrix.pk", 'wb')
+        pickle.dump(embedding_matrix, wf)
+        wf.close()
+
+    def read_ids(self, file):
+        rf = open("../example/" + file, 'rb')
+        return pickle.load(rf)
+
+
+if __name__ == '__main__':
+    e = EmbeddingReader()
+    word_index = e.read_ids("word_index.pk")
+
+    e.trim_word_embedding(word_index=word_index)
