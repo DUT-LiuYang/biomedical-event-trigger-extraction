394e474de98b8be2b998cc9a6fc199464f6e2fc9
DUT-LiuYang <1396924332@qq.com>
2018-10-18 20:44:57 +0800

    假期前的push，在基类中加入加载实例的代码

--- Models/BaseModel.py
+++ Models/BaseModel.py
@@ -1,15 +1,19 @@
-class Model:
+import pickle
+
+class BaseModel:
 
     def __init__(self):
 
         self.model = None
         self.max_len = 100
+        self.num_words = 6855
 
         self.embedding_matrix = None
         self.embedding_trainable = False
         self.EMBEDDING_DIM = 200
 
         self.save_dir = "../saved_models/"
+        self.dir = "../example/"
 
     def build_model(self):
         pass
@@ -22,3 +26,23 @@ class Model:
 
     def save_model(self):
         pass
+
+    def load_data(self, train=True):
+        if train:
+            path = self.dir + "train_"
+        else:
+            path = self.dir + "test_"
+
+        rf = open(path + "input.pk", 'rb')
+        word_inputs = pickle.load(rf)
+        rf.close()
+
+        rf = open(path + "entity_inputs.pk", 'rb')
+        entity_inputs = pickle.load(rf)
+        rf.close()
+
+        rf = open(path + "labels.pk", 'rb')
+        labels = pickle.load(rf)
+        rf.close()
+
+        return word_inputs, entity_inputs, labels
