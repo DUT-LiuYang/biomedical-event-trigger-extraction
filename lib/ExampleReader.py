6e9f6843860d3eb66f8b100fe8a1988c5a50c8a9
DUT-LiuYang <1396924332@qq.com>
2018-12-11 15:48:54 +0800

    Add code for generating the label for attention in my bibm 2017 paper.

--- lib/ExampleReader.py
+++ lib/ExampleReader.py
@@ -132,6 +132,211 @@ class ExampleReader(object):
 
         return labels
 
+    def get_attention_label(self,
+                            charoffset_file="",
+                            interaction_info_file="",
+                            max_len=125,):
+
+        duplicated_dict = self.get_duplicated_dict()
+
+        attention_label = []
+
+        token_offsets = []
+        tri_offsets = []
+        entity_offsets = []
+        tri_ids = []
+        entity_ids = []
+
+        interaction_e1 = []
+        interaction_e2 = []
+        interaction_type = []
+
+        rf = open(self.dir + charoffset_file, 'r', encoding='utf-8')
+        while True:
+            line = rf.readline()
+            if line == "":
+                break
+            line = line.split("#")
+            token_offsets.append(line[0].strip().split())
+            tri_offsets.append(line[1].strip().split())
+            tri_ids.append((line[2].strip().split()))
+            entity_offsets.append((line[3].strip().split()))
+            entity_ids.append(line[4].strip().split())
+        rf.close()
+
+        print(np.array(token_offsets).shape)
+
+        rf = open(self.dir + interaction_info_file, 'r', encoding='utf-8')
+        while True:
+            line = rf.readline()
+            if line == "":
+                break
+            line = line.split("#")
+            interaction_e1.append(line[0].strip().split())
+            interaction_e2.append(line[1].strip().split())
+            interaction_type.append(line[2].strip().split())
+        rf.close()
+
+        # construct attention label. [sen_num, max_len, max_len - 1]
+        sen_num = len(interaction_type)
+        for i in range(sen_num):
+            attention_label.append([])
+
+            # get the trigger labels sequence
+            tri_id_label, tri_offsets_ids = self.get_id_label_type(char_offsets=token_offsets[i], offsets=tri_offsets[i], ids=tri_ids[i])
+            tri_id_label = tri_id_label.strip().split()
+            # get the entity labels sequence
+            entity_id_label, _ = self.get_id_label_type(char_offsets=token_offsets[i], offsets=entity_offsets[i], ids=entity_ids[i])
+            entity_id_label = entity_id_label.strip().split()
+
+            # get the num of the entities and triggers in the current sentence.
+            tri_attention_label, num1 = self.get_average_attention(tri_id_label, entity_ids[i])
+            entity_attention_label, num2 = self.get_average_attention(entity_id_label, entity_ids[i])
+            num = num1 + num2
+            if num == 0:
+                ave_score = 0
+            else:
+                ave_score = 1.0 / num
+
+            for j in range(max_len):
+                attention_label[i].append([])
+                if j >= len(token_offsets[i]):
+                    attention_label[i][j] = [0] * max_len  # the padding part gets 0 attention.
+                elif tri_offsets_ids.get(token_offsets[i][j]) is None:
+
+                    for k in range(max_len):
+                        # the padding part and the current word get 0 attention.
+                        if k >= len(tri_attention_label) or j == k:
+                            attention_label[i][j].append(0)
+                        elif tri_attention_label[k] == 0 and entity_attention_label[k] == 0:
+                                attention_label[i][j].append(0)
+                        else:
+                            # print(str(score))
+                            attention_label[i][j].append(ave_score)
+                else:
+                    current_id = tri_offsets_ids.get(token_offsets[i][j])
+                    tri_attention_label, num1 = self.get_attention_label_type(tri_id_label, current_id, interaction_e1[i], interaction_e2[i], duplicated_dict, token_offsets[i][j])
+                    entity_attention_label, num2 = self.get_attention_label_type(entity_id_label, current_id, interaction_e1[i], interaction_e2[i], duplicated_dict, token_offsets[i][j])
+                    num = num1 + num2
+                    # if this sentence doesn't contain a entity or trigger except the current entity.
+                    if num == 0:
+                        score = 0
+                    else:
+                        score = 1.0 / num
+                    for k in range(max_len):
+                        if k >= len(tri_attention_label) or j == k:
+                            attention_label[i][j].append(0)
+                        elif tri_attention_label[k] == 0 and entity_attention_label[k] == 0:
+                            attention_label[i][j].append(0)
+                        else:
+                            # print(str(score))
+                            attention_label[i][j].append(score)
+
+        wf = open(self.output_file + "attention_label.pk", 'wb')
+        pickle.dump(attention_label, wf)
+        wf.close()
+
+        return attention_label
+
+    def get_id_label_type(self, char_offsets, offsets, ids):
+        label = ""
+        j = 0
+        signal = False
+        offset_ids = {}
+
+        if len(offsets) == 0:
+            for i in range(len(char_offsets)):
+                label += "O "
+            return label, offset_ids
+
+        for i in range(len(char_offsets)):
+            if j < len(offsets):
+                s1 = int(char_offsets[i].split("-")[0])
+                e1 = int(char_offsets[i].split("-")[1])
+                s2 = int(offsets[j].split("-")[0])
+                e2 = int(offsets[j].split("-")[1])
+                if s1 >= s2 and e1 <= e2:
+                    if signal and e1 == e2:
+                        label_type = "E-" + ids[j]
+                        offset_ids[char_offsets[i]] = ids[j]
+                        j += 1
+                        signal = False
+                    elif signal:
+                        label_type = "I-" + ids[j]
+                        offset_ids[char_offsets[i]] = ids[j]
+                    elif e1 == e2 and s1 == s2:
+                        label_type = "S-" + ids[j]
+                        offset_ids[char_offsets[i]] = ids[j]
+                        j += 1
+                    else:
+                        label_type = "B-" + ids[j]
+                        offset_ids[char_offsets[i]] = ids[j]
+                        signal = True
+                    label += " " + label_type
+                else:
+                    label += " O"
+            else:
+                label += " O"
+        # print(label)
+        return label, offset_ids
+
+    # however, entity that consists of several words need one attention?
+    def get_average_attention(self, id_label=[], interaction_e2=[]):
+        attention_label = []
+        num = 0
+        for i in range(len(id_label)):
+            attention_label.append(0)
+            for j in range(len(interaction_e2)):
+                if interaction_e2[j] == id_label[i][2:]:
+                    num += 1
+                    attention_label[i] = 1
+                    break
+        return attention_label, num
+
+    def get_attention_label_type(self, id_label=[], current_id="", interaction_e1=[], interaction_e2=[], duplicated_dict={}, offset=""):
+        attention_label = []
+        num = 0
+        # finish origin labeling
+        for i in range(len(id_label)):
+            attention_label.append(0)
+            for j in range(len(interaction_e2)):
+                # print(current_id + "-" + interaction_e1[j] + "-" + interaction_e2[j] + "-" + id_label[i][2:])
+                if current_id == interaction_e1[j] and interaction_e2[j] == id_label[i][2:]:
+                    num += 1
+                    attention_label[i] = 1
+                    break
+
+        # if the trigger has brothers with different ids...
+        if offset in duplicated_dict.keys():
+            duplicated_ids = duplicated_dict[offset].strip().split("#")
+            for ids in duplicated_ids:
+                for i in range(len(id_label)):
+                    for j in range(len(interaction_e2)):
+                        # print(current_id + "-" + interaction_e1[j] + "-" + interaction_e2[j] + "-" + id_label[i][2:])
+                        if ids == interaction_e1[j] and interaction_e2[j] == id_label[i][2:]:
+                            if attention_label[i] == 0:
+                                num += 1
+                            attention_label[i] = 1
+                            break
+
+        # print("===" + str(id_label))
+        # print("+++" + current_id + " " + str(num))
+        return attention_label, num
+
+    def get_duplicated_dict(self):
+        rf = open(self.output_file + "duplicated.txt", 'r', encoding='utf-8')
+        duplicated_dict = {}
+        while True:
+            line = rf.readline()
+            if line == "":
+                break
+            line = line.split("*")
+            key = line[0]
+            value = line[1]
+            duplicated_dict[key] = value
+        rf.close()
+        return duplicated_dict
+
 
 if __name__ == '__main__':
     e = ExampleReader(max_len=125, train=True)
@@ -146,6 +351,9 @@ if __name__ == '__main__':
     train_labels = e.get_label(train_labels, tri_class_id, class_num=73)
     train_entity_inputs = e.get_entity_input(train_entity_labels, entity_class_ids)
 
+    train_attention_label = e.get_attention_label(charoffset_file="train_offset_id.txt",
+                                                  interaction_info_file="train_interaction.txt")
+
     e = ExampleReader(max_len=125, train=False)
     test_inputs, test_labels, test_entity_labels, test_deps = e.get_data()
     test_labels = e.get_label(test_labels, tri_class_id, class_num=73)
@@ -154,3 +362,6 @@ if __name__ == '__main__':
     wf = open("../example/tri_index_ids.pk", 'wb')
     pickle.dump(tri_index_ids, wf)
     wf.close()
+
+    test_attention_label = e.get_attention_label(charoffset_file="test_offset_id.txt",
+                                                 interaction_info_file="test_interaction.txt")
