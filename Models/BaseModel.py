17d127cdb172396090b4210c5d16b8f9ea542d41
DUT-LiuYang <1396924332@qq.com>
2018-12-25 15:27:52 +0800

    some fix.

--- Models/BaseModel.py
+++ Models/BaseModel.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class BaseModel:
 
-    def __init__(self):
+    def __init__(self, use_development_set=True):
 
         # used dirs
         self.save_dir = "../saved_models/"
@@ -30,7 +30,7 @@ class BaseModel:
         self.ENTITY_TYPE_VEC_DIM = 50
 
         # inputs to the model
-        self.use_development_set = True
+        self.use_development_set = use_development_set
         self.train_word_inputs, self.train_entity_inputs, self.train_labels, self.train_attention_labels = self.load_data(train=True)
         self.test_word_inputs, self.test_entity_inputs, self.test_labels, self.test_attention_labels = self.load_data(train=False)
         self.dev_word_inputs, self.dev_entity_inputs, self.dev_labels, self.dev_attention_labels = [None, None, None, None]
